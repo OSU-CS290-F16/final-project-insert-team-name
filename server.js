8e8a89d752accad04a0e172a63eef55e9a51506a
ASladek <sladeka@oregonstate.edu>
2016-11-30 16:14:47 -0800

    Divided page into partials

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+var dogPix = require('./dog-pix');
 var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -9,8 +10,12 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+// Render the index page for the root URL path ('/').
+app.get('/', function (req, res) {
+  res.render('index-page', {
+      dogPix: dogPix
+    });
+});
 
 
 
