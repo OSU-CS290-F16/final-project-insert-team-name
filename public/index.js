ed03b5a23c0e4617c5712d551dbeb7cb2c7ba028
narxy <cranery@oregonstate.edu>
2016-12-06 19:46:15 -0800

    workin' hard

--- public/index.js
+++ public/index.js
@@ -1,4 +1,12 @@
 document.body.addEventListener('click',function(event){
-  if (event.target.className=="navbar-A")
-      document.body.getElementById('add-photo-modal').classList.toggle('sneaky')
+  console.log(event.target);
+  if (event.target.className=="add-button")
+      document.getElementById('add-photo-modal').classList.toggle('sneaky');
+
+  if (event.target.className=="modal-cancel"){
+      document.getElementById('new-photo-url').innerHTML="";
+      document.getElementById('new-photo-comment').innerHTML="";
+      document.getElementById('add-photo-modal').classList.toggle('sneaky');
+
+  }
 });
